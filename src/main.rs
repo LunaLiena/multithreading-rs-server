00f1ae48bb26f5d8d1b02a49307600e84b7a2a59
Lonny <radmir0099lol@gmail.com>
2024-11-28 23:17:58 +0300

    Feat/Check commit's and repo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+fn test_func_for_check_commits(num: i32) {
+    for i in 0..num {
+        print!("{}, ", i);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    test_func_for_check_commits(14);
 }
